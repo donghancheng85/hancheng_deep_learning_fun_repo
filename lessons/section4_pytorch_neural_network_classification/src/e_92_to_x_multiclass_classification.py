5ff4b68df8968c3115d62a200b6e2db94727d756
donghancheng85 <donghancheng85@gmail.com>
2026-03-04 21:09:21 -0500

    multi-class classification training loop

--- lessons/section4_pytorch_neural_network_classification/src/e_92_to_x_multiclass_classification.py
+++ lessons/section4_pytorch_neural_network_classification/src/e_92_to_x_multiclass_classification.py
@@ -37,7 +37,7 @@ X_blob, y_blob = make_blobs(
 
 # 2. turn data into tensors
 X_blob = torch.from_numpy(X_blob).type(torch.float32).to(device)
-y_blob = torch.from_numpy(y_blob).type(torch.float32).to(device)
+y_blob = torch.from_numpy(y_blob).type(torch.long).to(device)
 
 # 3. Split into and training and test
 X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(
@@ -47,7 +47,7 @@ X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(
 X_blob_train: torch.Tensor
 X_blob_test: torch.Tensor
 y_blob_train: torch.Tensor
-y_blob_train: torch.Tensor
+y_blob_test: torch.Tensor
 
 # 4. visualize the data
 plt.figure(figsize=(10, 7))
@@ -115,13 +115,68 @@ print(f"Shape of logits_before_training is {y_logits_before_training.shape}")
 
 # Conver out model's logts to prediction probabilities
 # softmax dim=1 means on dim 1, which represent the class probability sum (row) will be 1
-y_prediction_probability_before_training = torch.softmax(y_logits_before_training, dim=1)
+y_prediction_probability_before_training = torch.softmax(
+    y_logits_before_training, dim=1
+)
 print(y_prediction_probability_before_training[:5])
 
 # Conver model prediction probabilities to prediction labels
-y_prediction_labels_before_training = torch.argmax(torch.softmax(y_logits_before_training, dim=1), dim=1)
-print(f"y_prediction_labels_before_training shape is {y_prediction_labels_before_training.shape}")
+y_prediction_labels_before_training = torch.argmax(
+    torch.softmax(y_logits_before_training, dim=1), dim=1
+)
+print(
+    f"y_prediction_labels_before_training shape is {y_prediction_labels_before_training.shape}"
+)
 
 """
 8.5 Create a training loop and testing loop for multi-class classification
 """
+# Fit the multi-class model to the data
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+# Set number of epochs
+epochs = 100
+
+# Data to target device
+# X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device) # already done above so no need
+
+# Training/Testing Loop
+for epoch in range(epochs):
+    ### Training
+    blob_model.train()
+
+    # forward
+    y_logits_train = blob_model(X_blob_train)
+    y_predict_label_train = torch.argmax(torch.softmax(y_logits_train, dim=1), dim=1)
+
+    # calculate the loos
+    loss_train: torch.Tensor = loss_fn_crossentropy(y_logits_train, y_blob_train)
+    accuracy_train = accuracy_fn(y_true=y_blob_train, y_pred=y_predict_label_train)
+
+    # zero grad
+    optimizer_sdg.zero_grad()
+
+    # loss backward
+    loss_train.backward()
+
+    # optimizer step
+    optimizer_sdg.step()
+
+    ### Testing
+    blob_model.eval()
+    with torch.inference_mode():
+        y_logits_test = blob_model(X_blob_test)
+        y_predict_label_test = torch.softmax(y_logits_test, dim=1).argmax(
+            dim=1
+        )  # another way to get the label
+
+        loss_test: torch.Tensor = loss_fn_crossentropy(y_logits_test, y_blob_test)
+        accuracy_test = accuracy_fn(y_true=y_blob_test, y_pred=y_predict_label_test)
+
+        if epoch % 10 == 0:
+            print(
+                f"Epoch: {epoch} | "
+                f"Training loss: {loss_train:.5f}, Training accuracy: {accuracy_train:.2f}% | "
+                f"Test loss: {loss_test:.5f}, Tesing accuracy: {accuracy_test:.2f}%"
+            )
