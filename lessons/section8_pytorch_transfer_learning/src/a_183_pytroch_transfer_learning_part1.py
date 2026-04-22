9f538aee1543bcd4d3fcbc60c97c1b647f200714
donghancheng85 <donghancheng85@gmail.com>
2026-04-21 20:13:59 -0400

    create transform for pretrained model, will using EfficientNet_B0

--- lessons/section8_pytorch_transfer_learning/src/a_183_pytroch_transfer_learning_part1.py
+++ lessons/section8_pytorch_transfer_learning/src/a_183_pytroch_transfer_learning_part1.py
@@ -1,7 +1,8 @@
-from zipfile import Path
+from pathlib import Path
 
 import torch
 import torchvision
+from torchvision.transforms import v2
 
 # Continue with regular imports
 import matplotlib.pyplot as plt
@@ -40,3 +41,70 @@ data_path = Path("going_modular/data/")
 image_path = data_path / "pizza_steak_sushi"
 train_dir = image_path / "train"
 test_dir = image_path / "test"
+
+
+"""
+8.2 Create datasets and DataLoaders
+
+Note the transform, need to fit the pre-trained model's expected input size (e.g. 224x224 for ResNet, 299x299 for Inception).
+
+How to transform the data?
+With current version torchvision, two ways to do it:
+1. Manually create a transform pipeline using torchvision.transforms.Compose and apply it to the dataset.
+2. Automatically create - the transforms are defined by the model you want to use, you can get them from torchvision.models.get_model_weights 
+and torchvision.models.get_model_transforms.
+
+Important: when using pre-trained models, it's important that the data (include custom data) is transformed 
+in the same way that the data the pre-trained model was trained on was transformed. This is because the pre-trained 
+model has learned to recognize patterns in the data based on the specific transformations applied during its training. 
+If your custom data is not transformed in the same way, the pre-trained model may not perform well on it.
+"""
+
+"""
+8.2.1 Manually create a transform pipeline using torchvision.transforms.Compose and apply it to the dataset.
+torchvison.models has pre-trained models
+"""
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+# resize -> to tensor -> normalize
+manual_transform = v2.Compose(
+    [
+        v2.Resize((224, 224)),
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+    ]
+)
+
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=str(train_dir),
+    test_dir=str(test_dir),
+    train_transform=manual_transform,
+    test_transform=manual_transform,
+    batch_size=32,
+)
+
+print(f"Class names: {class_names}")
+print(f"Number of training batches: {len(train_dataloader)}")
+print(f"Number of testing batches: {len(test_dataloader)}")
+
+"""
+8.2.2 Automatically create - the transforms are defined by the model you want to use, you can get them from torchvision.models.get_model_weights 
+and torchvision.models.get_model_transforms.
+"""
+# get a set of pre-trained weights for a model (EfficientNet_B0 in this case)
+weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
+auto_transforms = weights.transforms()
+print(f"Auto transforms: {auto_transforms}")
+
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=str(train_dir),
+    test_dir=str(test_dir),
+    train_transform=auto_transforms,
+    test_transform=auto_transforms,
+    batch_size=32,
+)
+print(f"Class names: {class_names}")
+print(f"Number of training batches: {len(train_dataloader)}")
+print(f"Number of testing batches: {len(test_dataloader)}")
