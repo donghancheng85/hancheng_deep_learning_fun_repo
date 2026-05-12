345145b116fc5f51f9035e19eec2d9b1d55b74a3
donghancheng85 <donghancheng85@gmail.com>
2026-05-11 20:23:52 -0400

    create dataloader, model generate functions

---
+++ lessons/section9_pytorch_experiment_tracking/common/common_functions.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torchvision.transforms import v2
+from pathlib import Path
+from torch.utils.tensorboard import SummaryWriter
+
+from torch import nn
+from torchvision import transforms
+from datetime import datetime
+import os
+
+from torchinfo import summary
+
+from going_modular.pytorch_project import data_setup, engine, download_data
+from common.device import get_best_device, print_device_info
+from common.helper_fucntion import accuracy_fn, set_seeds, create_summary_writer
+
+"""
+7.5 Create functions to create efficientnet_b0 and efficientnet_b2 models with pretrained weights and custom classifier heads
+And the models should be returned ready to be trained, with a custom classifier head and the base layers frozen.
+To avoid retraining the model from pervious experiments.
+"""
+
+
+def create_efficientnet_b2_model(num_classes: int = 3) -> torch.nn.Module:
+    """Creates an efficientnet_b2 model with pretrained weights and a custom classifier head.
+
+    Args:
+        num_classes (int, optional): The number of output classes for the classifier head. Defaults to 3.
+
+    Returns:
+        torch.nn.Module: An efficientnet_b2 model with pretrained weights and a custom classifier head.
+    """
+    # get the weights and transforms for efficientnet_b2
+    weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT
+
+    # Create the model with pretrained weights
+    model = torchvision.models.efficientnet_b2(weights=weights)
+
+    # Freeze the base layers of the model
+    for param in model.features.parameters():
+        param.requires_grad = False
+
+    # Replace the classifier head with a custom one (dropout + linear layer with num_classes output features)
+    in_features: int = model.classifier[-1].in_features  # type: ignore[union-attr]
+    model.classifier[-1] = nn.Linear(in_features=in_features, out_features=num_classes)
+
+    model.__class__.__name__ = "efficientnet_b2"
+    print(
+        f"Created model: {model.__class__.__name__} with {sum(p.numel() for p in model.parameters() if p.requires_grad)} trainable parameters."
+    )
+
+    return model
+
+
+def create_efficientnet_b0_model(num_classes: int = 3) -> torch.nn.Module:
+    """Creates an efficientnet_b0 model with pretrained weights and a custom classifier head.
+
+    Args:
+        num_classes (int, optional): The number of output classes for the classifier head. Defaults to 3.
+
+    Returns:
+        torch.nn.Module: An efficientnet_b0 model with pretrained weights and a custom classifier head.
+    """
+    # get the weights and transforms for efficientnet_b0
+    weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
+
+    # Create the model with pretrained weights
+    model = torchvision.models.efficientnet_b0(weights=weights)
+
+    # Freeze the base layers of the model
+    for param in model.features.parameters():
+        param.requires_grad = False
+
+    # Replace the classifier head with a custom one (dropout + linear layer with num_classes output features)
+    in_features: int = model.classifier[-1].in_features  # type: ignore[union-attr]
+    model.classifier[-1] = nn.Linear(in_features=in_features, out_features=num_classes)
+
+    model.__class__.__name__ = "efficientnet_b0"
+    print(
+        f"Created model: {model.__class__.__name__} with {sum(p.numel() for p in model.parameters() if p.requires_grad)} trainable parameters."
+    )
+
+    return model
--- lessons/section9_pytorch_experiment_tracking/src/b_209_pytorch_experiment_tracking_part_2.py
+++ lessons/section9_pytorch_experiment_tracking/src/b_209_pytorch_experiment_tracking_part_2.py
@@ -16,6 +16,11 @@ from going_modular.pytorch_project import data_setup, engine, download_data
 from common.device import get_best_device, print_device_info
 from common.helper_fucntion import accuracy_fn, set_seeds, create_summary_writer
 
+from lessons.section9_pytorch_experiment_tracking.common.common_functions import (
+    create_efficientnet_b0_model,
+    create_efficientnet_b2_model,
+)
+
 # set up device
 device = get_best_device()
 print_device_info(device)
@@ -73,3 +78,103 @@ This is why transfer learning is so useful, it allows us to quickly iterate thro
 10% dir - going_modular/data/pizza_steak_sushi
 20% dir - lessons/section6_pytorch_custom_datasets/data/pizza_steak_sushi_increased
 """
+
+"""
+7.4 Transform the data and create DataLoaders
+
+1. Resize (224, 224) - required for efficientnet_b0 and efficientnet_b2
+2. Convert to tensor, value between [0, 1]
+3. Normalize (efficientnet_b0 and efficientnet_b2 were trained on imagenet, so we will use the imagenet mean and std for normalization)
+"""
+# Set up paths
+data_path_10_percent = Path("going_modular/data/pizza_steak_sushi")
+data_path_20_percent = Path(
+    "lessons/section6_pytorch_custom_datasets/data/pizza_steak_sushi_increased"
+)
+
+# Set up training dir
+train_dir_10_percent = data_path_10_percent / "train"
+train_dir_20_percent = data_path_20_percent / "train"
+
+# Set up test dir (same for both)
+test_dir = data_path_10_percent / "test"
+
+print(f"Training directory for 10% data: {train_dir_10_percent}")
+print(f"Training directory for 20% data: {train_dir_20_percent}")
+print(f"Testing directory: {test_dir}")
+
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+BATCH_SIZE = 32
+
+# Set up transforms (same for both)
+manual_transform = v2.Compose(
+    [
+        v2.Resize((224, 224)),
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+    ]
+)
+
+train_dataloader_10_percent, test_dataloader, class_names = (
+    data_setup.create_dataloaders(
+        train_dir=str(train_dir_10_percent),
+        test_dir=str(test_dir),
+        train_transform=manual_transform,
+        test_transform=manual_transform,
+        batch_size=BATCH_SIZE,
+    )
+)
+
+train_dataloader_20_percent, _, _ = data_setup.create_dataloaders(
+    train_dir=str(train_dir_20_percent),
+    test_dir=str(test_dir),
+    train_transform=manual_transform,
+    test_transform=manual_transform,
+    batch_size=BATCH_SIZE,
+)
+
+print(
+    f"Number of batches in 10% training dataloader: {len(train_dataloader_10_percent)}"
+)
+print(
+    f"Number of batches in 20% training dataloader: {len(train_dataloader_20_percent)}"
+)
+print(f"Number of batches in test dataloader: {len(test_dataloader)}")
+print(f"Class names: {class_names}")
+
+# Use create_efficientnet_b0_model() and create_efficientnet_b2_model() to create models
+efficientnet_b0_model = create_efficientnet_b0_model(num_classes=len(class_names)).to(
+    device
+)
+efficientnet_b2_model = create_efficientnet_b2_model(num_classes=len(class_names)).to(
+    device
+)
+
+summary(
+    model=efficientnet_b0_model,
+    input_size=(1, 3, 224, 224),
+    col_names=[
+        "input_size",
+        "output_size",
+        "num_params",
+        "trainable",
+    ],
+    col_width=20,
+    row_settings=["var_names"],
+)
+
+summary(
+    model=efficientnet_b2_model,
+    input_size=(1, 3, 224, 224),
+    col_names=[
+        "input_size",
+        "output_size",
+        "num_params",
+        "trainable",
+    ],
+    col_width=20,
+    row_settings=["var_names"],
+)
